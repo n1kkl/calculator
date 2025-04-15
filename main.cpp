01bc4ab7fc175506a870f0074f9b36323142d601
Niklas <niklas@nikkl.me>
2025-04-15 22:20:36 +0200

    feat(repl): measure and display lexing time for input

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
+#include <chrono>
 #include <iostream>
 
 #include "lexer.h"
 
 [[noreturn]] int main() {
     while (true) {
-        std::cout << ">> ";
+        std::cout << ">>  ";
         std::string line;
         std::getline(std::cin, line);
+
+        auto start = std::chrono::high_resolution_clock::now();
+
         Lexer lexer(line);
+        std::vector<Token> tokens = std::vector<Token>();
+
+        for (Token token = lexer.next_token(); token.type != TokenType::End; token = lexer.next_token()) {
+            tokens.push_back(token);
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
         std::cout << "";
-        for (Token token = lexer.next_token(); token.type != TokenType::End; token = lexer.next_token()) {
+        for (const Token& token: tokens) {
             std::cout << token << " ";
         }
         std::cout << std::endl;
+
+        std::cout << "lexing time: " << microseconds << std::endl;
     }
 }
