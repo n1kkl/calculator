e0701ef7eec9cb3257aababf5cb47ff60f3a262d
Niklas <niklas@nikkl.me>
2025-04-15 22:34:34 +0200

    fix(lexer): fix issue where illegal literals are always empty strings

--- lexer.cpp
+++ lexer.cpp
@@ -1,80 +1,78 @@
-#include <memory>
 #include <utility>
 #include "lexer.h"
 
 Lexer::Lexer(std::string input) : input(std::move(input)) {
-  current_char = 0;
-  read_char();
+    current_char = 0;
+    read_char();
 }
 
 void Lexer::read_char() {
     if (read_position >= input.length()) {
-      current_char = 0;
+        current_char = 0;
     } else {
-      current_char = input[read_position];
+        current_char = input[read_position];
     }
     position = read_position;
     read_position += 1;
 }
 
 Token Lexer::next_token() {
-  Token token(TokenType::Illegal, std::string(1, current_char));
+    skip_whitespace();
+    Token token(TokenType::Illegal, std::string(1, current_char));
 
-  skip_whitespace();
+    switch (current_char) {
+        case '+':
+            token = Token(TokenType::Add, "+");
+            break;
+        case '-':
+            token = Token(TokenType::Sub, "-");
+            break;
+        case '*':
+            token = Token(TokenType::Mul, "*");
+            break;
+        case '/':
+            token = Token(TokenType::Div, "/");
+            break;
+        case '(':
+            token = Token(TokenType::LParen, "(");
+            break;
+        case ')':
+            token = Token(TokenType::RParen, ")");
+            break;
+        case 0:
+            token = Token(TokenType::End, "");
+            break;
+        default:
+            if (is_number(current_char)) {
+                token = Token(TokenType::Number, read_number());
+                return token;
+            }
+            break;
+    }
 
-  switch (current_char) {
-    case '+':
-      token = Token(TokenType::Add, "+");
-      break;
-    case '-':
-      token = Token(TokenType::Sub, "-");
-      break;
-    case '*':
-      token = Token(TokenType::Mul, "*");
-      break;
-    case '/':
-      token = Token(TokenType::Div, "/");
-      break;
-    case '(':
-      token = Token(TokenType::LParen, "(");
-      break;
-    case ')':
-      token = Token(TokenType::RParen, ")");
-      break;
-    case 0:
-      token = Token(TokenType::End, "");
-      break;
-    default:
-      if (is_number(current_char)) {
-        token = Token(TokenType::Number, read_number());
-        return token;
-      }
-      break;
-  }
+    read_char();
 
-  read_char();
-
-  return token;
+    return token;
 }
 
 bool Lexer::is_whitespace(const char token) {
-  return token == ' ' || token == '\t' || token == '\n' || token == '\r';
+    return token == ' ' || token == '\t' || token == '\n' || token == '\r';
 }
 
 void Lexer::skip_whitespace() {
-  while (is_whitespace(current_char)) {
-    read_char();
-  }
+    while (is_whitespace(current_char)) {
+        read_char();
+    }
 }
 
 bool Lexer::is_number(const char token) {
-  return token >= '0' && token <= '9';
+    return token >= '0' && token <= '9';
 }
 
 std::string Lexer::read_number() {
-  int from_position = position;
-  while (is_number(current_char)) {
-    read_char();
-  }
-  return input.substr(from_position, position - from_position);
+    int from_position = position;
+    while (is_number(current_char)) {
+        read_char();
+    }
+    return input.substr(from_position, position - from_position);
 }
